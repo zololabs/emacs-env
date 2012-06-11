b4e6182d7404f9671ad56c22607e19f2a636b3dd
Siva Jagadeesan <sivajag@gmail.com>
2012-06-11 15:18:18 -0700

    updated theme

--- init.el
+++ init.el
@@ -49,3 +49,16 @@
 
 ;;; Maximize Frame
 (maximize-frame)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("0c8ffb272e65e2ffc49b0d5755d7db137931c5e3ed47890d7a6de07717eaa2e8" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
